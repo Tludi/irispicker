20a775b843764905c48e205a9a21f25b3dabf399
Tludi <timothyludi@diligentagility.com>
2019-03-18 14:27:11 -0700

    initial commit

---
+++ scripts/IrisPicker.js
@@ -0,0 +1,34 @@
+
+
+function resetSquareColours() {
+    for (var i = 0; i < 45; i++) {
+        // if (i == 12)
+            // scontinue;
+        var currentSquare = document.getElementById("sq" + i);
+        currentSquare.style.backgroundColor = "#ffffff";
+    }
+    return;
+}
+
+function markSquare(square) {
+    var currentSquare = document.getElementById(square);
+    if (currentSquare.style.backgroundColor == "lightblue") 
+        currentSquare.style.backgroundColor = "#ffffff";
+    else
+        currentSquare.style.backgroundColor = "lightblue";
+    return;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
